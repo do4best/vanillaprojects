51e579e2f4becdc88e35e1a0bcaf4571e75b2100
Meer Afzal <do4best@gmail.com>
2024-11-09 23:34:56 +0500

    The worling project is successufly complted

--- todolist/todo.js
+++ todolist/todo.js
@@ -1,21 +1,30 @@
-const listcontainer = document.getElementById("list-container")
-const inputBox = document.getElementById("input-box")
+const listcontainer = document.getElementById("list-container")// this points to the list items
+const inputBox = document.getElementById("input-box") // this points to the input box
 
-function addTask(){
-    if(inputBox.value === ''){
+function addTask(){ // the function is responsible for add the task
+    if(inputBox.value === ''){ // putting a condation if input value is null display the alert option
         alert("Enter Some Data")
     }else{
         let li = document.createElement("li")
         li.innerHTML = inputBox.value;
         listcontainer.appendChild(li)
+
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7";
+        li.appendChild(span)
+
     }
     inputBox.value = "";
     saveData()
 }
-listcontainer.addEventListener("click",function(e){
+listcontainer.addEventListener("keypress","click",function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked")
         saveData()
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove()
+        saveData()
+
     }
 })
 function saveData(){
