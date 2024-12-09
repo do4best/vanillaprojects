be475067e392f1b2c264bcacf4a5934905f6cf8f
Meer Afzal <do4best@gmail.com>
2024-12-09 23:02:18 +0500

    capitlize funtion is exercised

--- dsaUnderstanding/findthemax.js
+++ dsaUnderstanding/findthemax.js
@@ -1,13 +1,19 @@
-function findthemax(arr){
-    let max = arr[0]
-    for(let i=1; i<arr.length; i++){
-        if(arr[i] > max){
-            max = arr[i]
+const findMax=(arr)=>{
+    let mymax=arr[0];
+    for(let i=0; i < arr.length; i++){
+        if(arr[i] > mymax){
+            mymax = arr[i]
         }
     }
-    return max;
+    return mymax;
 }
-let arr1=[3,2,4,5,6,8,9]
-console.log(Math.max(...arr1))
-
-console.log(findthemax(arr1))
+let arr1=[5,8,9,10,12]
+console.log(findMax(arr1))
+function firstletterCapitalized(str){
+    let words = str.toLowerCase().split(' ')
+    for(let i=0; i<words.length; i++){
+        words[i] = words[i][0].toUpperCase() + words[i].slice(1) 
+    }
+    return words.join(' ')
+}
+console.log(firstletterCapitalized("This is StraNGE"))
