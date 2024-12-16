f17baf54c05707f3b7230c86070f5cc009feac89
Meer Afzal <do4best@gmail.com>
2024-12-16 23:26:43 +0500

    Practising stack

---
+++ understanddsa/stack.js
@@ -0,0 +1,46 @@
+class Stack{
+constructor(){
+this.stack = []
+this.maxSize = 100;
+this.top = -1
+}
+add(vlaue){
+if(this.isFull()){
+    return null;
+}
+this.top++
+this.stack[this.top] = vlaue;
+
+return true;
+}
+isFull(){
+    return this.top === this.maxSize-1;
+}
+remove(){
+    if(this.isEmpty()){
+        return null;
+    }
+   
+    this.top--;
+    return this.stack.pop()
+
+}
+isEmpty(){
+    return this.top === -1
+}
+peek(){
+if(this.isEmpty()){
+    return null
+}
+return this.stack[this.top]
+}
+}
+let stack = new Stack()
+console.log(stack)
+stack.add("Fuck")
+stack.add("Again")
+stack.add("Die")
+console.log(stack)
+stack.remove()
+console.log(stack)
+console.log(stack.peek())
