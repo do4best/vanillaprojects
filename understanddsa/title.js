422fe2fd2082d2f70c5c760cb3fcd4bdad07d3ab
Meer Afzal <do4best@gmail.com>
2024-12-31 23:46:43 +0500

    Duplicate array algo is practiced

--- understanddsa/title.js
+++ understanddsa/title.js
@@ -16,6 +16,17 @@ function anotherReverse(str){
 
     return reverse;
 }
+function removeDuplicat(arr){
+let arrlenght = [];
+for(let i=0; i<arr.length; i++){
+    if(!arrlenght.includes(arr[i])){
+        arrlenght.push(arr[i])
+    }
+
+}
+return arrlenght;
+}
+console.log(removeDuplicat([1,1,2,2,2,3,3,5,4,4,5]))
 let sent = "THIS is it"
 console.log(firstTitle(sent))
 console.log(reverseit("Hello"))
