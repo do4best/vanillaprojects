fa4572d4e81aba1c8d7b10be21e6a33b60a27aa0
Meer Afzal <do4best@gmail.com>
2025-01-01 23:11:40 +0500

    numberrange exaple is added.

--- understanddsa/makeitreverse.js
+++ understanddsa/makeitreverse.js
@@ -33,6 +33,14 @@ function sumit(arr){
     }
     return arr[0] + sumit(arr.slice(1))
 }
+function numberRange(start,end){
+    if(start === end){
+        return [start]
+    }
+    const result = numberRange(start,end - 1)
+    result.push(end)
+    return result;
+}
 countDown(20)
 let result = reverseString("Hello Friend") 
 console.log(result)
@@ -40,3 +48,4 @@ console.log(fibnacchi(10))
 console.log(factorial(10))
 console.log(raisedPower(2,10))
 console.log(sumit([2,3,4,5,6,7,8,9,10]))
+console.log(numberRange(1,5))
