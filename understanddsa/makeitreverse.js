1cab4260fca6ffa5c292ebc2eeb4fe02f14fb284
Meer Afzal <do4best@gmail.com>
2025-01-01 23:00:36 +0500

    recusrion is revisited.

---
+++ understanddsa/makeitreverse.js
@@ -0,0 +1,42 @@
+function countDown(num){
+    if(num <= 0 ){
+        return;
+    }
+    console.log(num)
+    num--;
+    countDown(num)
+}
+const reverseString=(str)=>{
+return str === ''?'':reverseString(str.substr(1)) + str.charAt(0)
+}
+function fibnacchi(item){
+    if(item === 1){
+        return 1;
+    }
+    return item + fibnacchi(item - 1)
+}
+function factorial(item){
+    if(item === 1 ){
+        return 1;
+    }
+    return item * factorial(item - 1)
+}
+function raisedPower(base,exp){
+    if(exp === 0){
+return 1;
+    }
+    return base * raisedPower(base,exp - 1)
+}
+function sumit(arr){
+    if(arr.length === 0){
+        return 0;
+    }
+    return arr[0] + sumit(arr.slice(1))
+}
+countDown(20)
+let result = reverseString("Hello Friend") 
+console.log(result)
+console.log(fibnacchi(10))
+console.log(factorial(10))
+console.log(raisedPower(2,10))
+console.log(sumit([2,3,4,5,6,7,8,9,10]))
