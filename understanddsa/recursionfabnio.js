e72b1c4f8ce89c1c6097d9280ba769a85b6ca6ff
Meer Afzal <do4best@gmail.com>
2025-01-15 23:15:23 +0500

    raised power to the asked power

--- understanddsa/recursionfabnio.js
+++ understanddsa/recursionfabnio.js
@@ -4,5 +4,11 @@ function fibnacchi(num){
         return fibnacchi(num-1) + fibnacchi(num-2)
     
 }
+function raisedPoser(base,exponent){
+    if( exponent === 0){
+        return 1;
+    }
+    return base * raisedPoser(base,exponent-1)}
 
+    console.log(raisedPoser(5,5))
 console.log(fibnacchi(5))
