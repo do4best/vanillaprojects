ed1abb31135432900315eeec167a77ddcaee1639
Meer Afzal <do4best@gmail.com>
2025-01-16 23:55:13 +0500

    understand stack again and its 5 method is implemented push(),pop(),isEmpty(),isFull(),peek()

--- understanddsa/classStack.js
+++ understanddsa/classStack.js
@@ -15,12 +15,28 @@ class Stack{
     isFull(){
         return this.top === this.maxSize - 1;
     }
+    pop(){
+        if(this.isEmpty()){
+            return null;
+        }
+     
+        this.top--;
+        return this.stack.pop();
+    }
     isEmpty(){
         return this.top === -1
+    }
+    peek(){
+        if(this.isEmpty()){
+            return null;
+        }
+        return this.stack[this.top];
     }
 }
 const stack = new Stack();
 stack.push('1')
 stack.push('2')
 stack.push('3')
+stack.pop()
 console.log(stack)
+console.log(stack.peek())
