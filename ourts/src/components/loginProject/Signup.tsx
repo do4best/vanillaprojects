f150c4923efc0b8b4ccfcfd0865420b872683eb1
Meer Afzal <do4best@gmail.com>
2025-03-11 23:33:11 +0500

    I have two issue in the programme first error which need to be solved is 'TypeError: e.type.context is undefined' and second unable to connect with database though it is working i mean its running

--- ourts/src/components/loginProject/Signup.tsx
+++ ourts/src/components/loginProject/Signup.tsx
@@ -1,23 +1,34 @@
+import axios from "axios";
+import { useState } from "react";
+
 function Signup() {
+    const [name,setName] = useState<string>("")
+    const [email,setEmail] = useState<string>("")
+    const [password,setPassword] = useState<string>("")
+    const handleSubmit=(e:React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault()
+        axios.post('https://localhost:3001/register',{name,email,password}).then(result=>console.log(result)).catch(err=>console.log(err))
+    }
     return ( <>
-    
-    <div className=" m-auto flex justify-center h-110 shadow-lg shadow-blue-500">
-   <fieldset className="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box">
-  <legend className="fieldset-legend text-3xl">Register</legend>
-  <label className="fieldset-label">Name</label>
-  <input type="email" className="input" placeholder="Enter your Name" />
-  
-  <label className="fieldset-label">Email</label>
-  <input type="email" className="input" placeholder="Email" />
-  
-  <label className="fieldset-label">Password</label>
-  <input type="password" className="input" placeholder="Password" />
-  
-  <button className="btn btn-neutral mt-4">Register</button>
-  <label className="fieldset-label">Already have a Account</label>
-  <button className="btn btn-neutral mt-4">Login</button>
-</fieldset>
-</div>
+     <div className="m-auto flex justify-center h-110 shadow-lg shadow-blue-500">
+                <form onSubmit={handleSubmit} className="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box">
+                    <fieldset>
+                        <legend className="fieldset-legend text-3xl">Register</legend>
+                        <label className="fieldset-label">Name</label>
+                        <input type="text" className="input" placeholder="Enter your Name" onChange={(e) => setName(e.target.value)} />
+
+                        <label className="fieldset-label">Email</label>
+                        <input type="email" className="input" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+
+                        <label className="fieldset-label">Password</label>
+                        <input type="password" className="input" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+
+                        <button type="submit" className="btn btn-neutral mt-4">Register</button>
+                        <label className="fieldset-label">Already have an Account</label>
+                        <button type="button" className="btn btn-neutral mt-4">Login</button>
+                    </fieldset>
+                </form>
+            </div>
     </> );
 }
 
---
+++ ourts/src/components/loginProject/server/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors'
+import mongoose from 'mongoose'
+import { EmployeeModel } from './models/employee.js'
+
+const app  = express()
+app.use(express.json())
+app.use(cors())
+mongoose.connect("mongodb://127.0.0.1:27017/myemployee");
+app.post('/register',(req,res)=>{
+EmployeeModel.create(req.body).then(employee =>res.json(employee)).catch(err=>req.json(err))    
+})
+app.listen(3001,()=>{
+    console.log("We are listeneing")
+})
